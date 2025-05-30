0971c02cbe1e43cf43e473b15ec004b82987c57d
DaphneCuadra <dap.cuadra@duocuc.cl>
2025-05-29 22:08:32 -0400

    Productos Nuevos

--- app/database.py
+++ app/database.py
@@ -4,8 +4,8 @@ def get_conexion():
     conexion = oracledb.connect(
         user="bd_clavitos",
         password="bd_clavitos",
-        dsn= "localhost:1521/XE",
+        #dsn= "localhost:1521/XE",
         #dsn="localhost:1521/orcl.duoc.com.cl"
-        #dsn= "192.168.1.93:1521/XE",
+        dsn= "192.168.1.93:1521/XE",
     )
     return conexion
